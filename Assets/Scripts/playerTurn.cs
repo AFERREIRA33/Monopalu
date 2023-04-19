588446b949e4b860b3cd94a921764b76c3042f7c
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-04-19 10:15:30 +0200

    add decor

---
+++ Assets/Scripts/playerTurn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class playerTurn : MonoBehaviour
+{
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("je suis le player");
+            gameManager.turn = false;
+        }
+    }
+}
