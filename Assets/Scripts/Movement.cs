d842b0b71119a1b37acee2778bdb4cc90a618285
joseph selva <daymoove@gmail.com>
2023-04-27 11:09:40 +0200

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject[] boxs;
+
+    public float movespeed = 1f;
+
+    public int boxIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxs = GameObject.FindGameObjectsWithTag("Case");
+        transform.position = boxs[boxIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Move(int rollnumber)
+    {
+        if (boxIndex + rollnumber > 39)
+        {
+            while (boxIndex <= 39)
+            {
+                boxIndex++;
+                rollnumber--;
+            }
+            boxIndex = 0;
+        }
+        boxIndex += rollnumber;
+
+        transform.position = boxs[boxIndex].transform.position;
+    }
+}
