545f87a65d7b975a0c0445a286f88496aa76d598
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-04-27 14:22:09 +0200

    sql

--- Assets/Scripts/Db.cs
+++ Assets/Scripts/Db.cs
@@ -1,123 +1,135 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Data;
-using Mono.Data.Sqlite;
-using Newtonsoft.Json.Linq;
-using static Unity.VisualScripting.Dependencies.Sqlite.SQLite3;
 using System;
-using System.Linq;
+using Mono.Data.Sqlite;
 
-public class dbAccess
+
+public class Db
 {
-    // variables for basic query access
-    private string connection;
-    private IDbConnection dbcon;
-    private IDbCommand dbcmd;
-    private IDataReader reader;
-   
-    public void OpenDB(string p)
+    private SqliteConnection dbConnection;
+    private SqliteCommand dbCommand;
+    private SqliteDataReader reader;
+
+    public Db(string connectionString)
     {
-        connection = "URI=file:" + p; // we set the connection to our database
-        dbcon = new SqliteConnection(connection);
-        dbcon.Open();
+        OpenDB(connectionString);
     }
 
-    public IDataReader BasicQuery(string q,bool r)
-    { // run a baic Sqlite query
-        dbcmd = dbcon.CreateCommand(); // create empty command
-        dbcmd.CommandText = q; // fill the command
-        reader = dbcmd.ExecuteReader(); // execute command which returns a reader
-        if (r)
-        { // if we want to return the reader
-            return reader; // return the reader
-        } else
+    public void OpenDB(string connectionString)
+    {
+        dbConnection = new SqliteConnection(connectionString);
+        dbConnection.Open();
+        Debug.Log("Connected to db");
+    }
+
+    public void CloseSqlConnection()
+    {
+        if (dbCommand != null)
         {
-            return null;
+            dbCommand.Dispose();
         }
+        dbCommand = null;
+        if (reader != null)
+        {
+            reader.Dispose();
+        }
+        reader = null;
+        if (dbConnection != null)
+        {
+            dbConnection.Close();
+        }
+        dbConnection = null;
+        Debug.Log("Disconnected from db.");
     }
 
-    public void CreateTable(string name, string[] col, string[] colType)
-    { // Create a table, name, column array, column type array
-        String query;
-        query = "CREATE TABLE " + name + "(" + col[0] + " " + colType[0];
-        for (var i = 1; i < col.Length; i++)
+    public SqliteDataReader ExecuteQuery(string sqlQuery)
+    {
+        dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = sqlQuery;
+
+        reader = dbCommand.ExecuteReader();
+
+
+        return reader;
+    }
+
+
+    public SqliteDataReader ReadFullTable(string tableName)
+    {
+        string query = "SELECT * FROM " + tableName;
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader InsertInto(string tableName, string[] values)
+    {
+        string query = "INSERT INTO " + tableName + " VALUES (" + values[0];
+        for (int i = 1; i < values.Length; ++i)
+        {
+            query += ", " + values[i];
+        }
+        query += ")";
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader InsertIntoSpecific(string tableName, string[] cols, string[] values)
+    {
+        if (cols.Length != values.Length)
+        {
+            throw new SqliteException("columns.Length != values.Length");
+        }
+        string query = "INSERT INTO " + tableName + "(" + cols[0];
+        for (int i = 1; i < cols.Length; ++i)
+        {
+            query += ", " + cols[i];
+        }
+        query += ") VALUES (" + values[0];
+        for (int i = 1; i < values.Length; ++i)
+        {
+            query += ", " + values[i];
+        }
+        query += ")";
+        return ExecuteQuery(query);
+    }
+
+    public SqliteDataReader DeleteContents(string tableName)
+    {
+        string query = "DELETE FROM " + tableName;
+        return ExecuteQuery(query);
+    }
+    public SqliteDataReader CreateTable(string name, string[] col, string[] colType)
+    {
+        if (col.Length != colType.Length)
+        {
+            throw new SqliteException("columns.Length != colType.Length");
+        }
+        string query = "CREATE TABLE " + name + " (" + col[0] + " " + colType[0];
+        for (int i = 1; i < col.Length; ++i)
         {
             query += ", " + col[i] + " " + colType[i];
         }
         query += ")";
-        dbcmd = dbcon.CreateCommand(); // create empty command
-        dbcmd.CommandText = query; // fill the command
-        reader = dbcmd.ExecuteReader(); // execute command which returns a reader
-
+        return ExecuteQuery(query);
     }
-
-    public void InsertIntoSingle(string tableName, String colName, String value)
-    { // single insert
-        String query;
-        query = "INSERT INTO " + tableName + "(" + colName + ") " + "VALUES (" + value + ")";
-        dbcmd = dbcon.CreateCommand(); // create empty command
-        dbcmd.CommandText = query; // fill the command
-        reader = dbcmd.ExecuteReader(); // execute command which returns a reader
-    }
-
-    public void InsertIntoSpecific(string tableName, Array[] col, Array[] values)
-    { // Specific insert with col and values
-        String query;
-        query = "INSERT INTO " + tableName + "(" + col[0];
-        for (int i = 1; i < col.Length; i++)
-        {
-            query += ", " + col[i];
-        }
-        query += ") VALUES (" + values[0];
-        for (int i = 1; i < values.Length; i++)
-        {
-            query += ", " + values[i];
-        }
-        query += ")";
-        dbcmd = dbcon.CreateCommand();
-        dbcmd.CommandText = query;
-        reader = dbcmd.ExecuteReader();
-    }
-
-    public void InsertInto(String tableName, int[] values)
-    { // basic Insert with just values
-        String query;
-        query = "INSERT INTO " + tableName + " VALUES (" + values[0];
-        for (var i = 1; i < values.Length; i++)
-        {
-            query += ", " + values[i];
-        }
-        query += ")";
-        dbcmd = dbcon.CreateCommand();
-        dbcmd.CommandText = query;
-        reader = dbcmd.ExecuteReader();
-    }
-
-    public void SingleSelectWhere(String tableName, String itemToSelect, String wCol, String wPar, String wValue)
-    { // Selects a single Item
-        String query;
-        query = "SELECT " + itemToSelect + " FROM " + tableName + " WHERE " + wCol + wPar + wValue;
-        dbcmd = dbcon.CreateCommand();
-        dbcmd.CommandText = query;
-        reader = dbcmd.ExecuteReader();
-        Array[] readArray;
-        while (reader.Read())
-        {
-            readArray.Append(readArray, reader.GetString(0)); // Fill array with all matches
-        }
-        return readArray; // return matches
-    }
-
-
-    public void CloseDB()
+    public SqliteDataReader SelectWhere(string tableName, string[] items, string[] col, string[] operation, string[] values)
     {
-        reader.Close(); // clean everything up
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbcon.Close();
-        dbcon = null;
+        if (col.Length != operation.Length || operation.Length != values.Length)
+        {
+            throw new SqliteException("col.Length != operation.Length != values.Length");
+        }
+        string query = "SELECT " + items[0];
+        for (int i = 1; i < items.Length; ++i)
+        {
+            query += ", " + items[i];
+        }
+        query += " FROM " + tableName + " WHERE " + col[0] + operation[0] + "'" + values[0] + "' ";
+        for (int i = 1; i < col.Length; ++i)
+        {
+            query += " AND " + col[i] + operation[i] + "'" + values[0] + "' ";
+        }
+
+        return ExecuteQuery(query);
+
     }
+
+
 
 }
