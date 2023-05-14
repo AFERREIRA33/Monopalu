f7b9b90f29e0a167dee69d43ec671379c08e7b9e
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-05-14 17:57:45 +0200

    change mmoney

--- Assets/Scripts/Money.cs
+++ Assets/Scripts/Money.cs
@@ -9,7 +9,7 @@ public class Money : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //money = 148000;
+        money = 148000;
         
     }
 
